16720bfcebab58eead59e63656c87fea3832bbb1
Fowyous <mohamed.koubaa@utbm.fr>
2025-11-20 14:39:47 +0100

    Update main.c

--- main.c
+++ main.c
@@ -3,7 +3,21 @@
 #include <sys/sysinfo.h>
 #include <stdlib.h>
 #include <stdbool.h>
+#include <stdio.h>
 
+void help(char* nom){
+	printf("Usage %s [Options]\n\n", nom);
+	printf("--help ou -h		afficher ce message et sortir\n\n");
+	printf("--dry-run		test l'accès à la liste des processus sur la machine locale et/ou distante sans les afficher\n\n");
+	printf("--remote-config ou -c	spécifie le chemin vers le fichier de configuration contenant les informations de connexion\n			sur les machines distantes\n\n");
+	printf("--connexiton-type ou -t spécifie le type de connexion à utiliser pour la connexion sur les machines distantes (ssh, telnet)\n\n");
+	printf("--port ou -P 		spécifie le port à utiliser pour le type de connexion choisi. Si cette option n'est pas spécifié,\n			alor le port par défaut du type de connexion est choisi\n\n");
+	printf("--login ou -l		spécifie l'identifiant de connexion et la machine distante. Ex : --login user@remote_server.\n			remote_server est soit l'IP ou le nom DNS de la machine distante\n\n");
+	printf("--remote-server ou -s	spécifie le nom DNS ou l'IP de la machine distante\n\n");
+	printf("--username ou -u	spécifie le nom de l'utilisateur à utiliser pour la connexion\n\n");
+	printf("--password ou -p	spécifie le mot de passe à utiliser pour la connexion\n\n");
+	printf("--all ou -a		spécifie au programme de collecter à la fois la liste des processus sur la machine local et les machines distantes.\n		S'utilise uniquement si l'option -c ou -s est utilisé.\n\n");
+}
 int main(int argc, char *argv[]) {
 	int opt = 0;
 	struct option my_opts[] = {
@@ -23,11 +37,11 @@ int main(int argc, char *argv[]) {
 	char* temp_directory = 0;
 	char* outf = 0;
 	bool verbose = 0;
-	while((opt = getopt_long(argc, argv, "", my_opts, 0)) != -1) {
+	while((opt = getopt_long(argc, argv, "h", my_opts, 0)) != -1) {
 		switch (opt) {
 			case 'h':
-				printf("option verbose\n");
-        break;
+				help(argv[0]);
+			       return 0;
 			case 'd'://dry run
 				break;
 			case 'c'://remote config
