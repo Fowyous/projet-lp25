f8f01e88ae3961465e7d319cfb573b839a1016d8
Fowyous <mohamed.koubaa@utbm.fr>
2025-11-21 20:37:27 +0100

    Update main.h

--- main.h
+++ main.h
@@ -1,3 +1,4 @@
+#define STR_MAX 1024 // le maximum de charactéres qu'on autorise pour un str
 typedef enum {
 	DRY_RUN,
 	REMOTE_CONF,
@@ -6,5 +7,23 @@ typedef enum {
 	LOGIN,
 	REMOTE_SERV,
 	USERNAME,
-	PASSWORD
-}
+	PASSWORD,
+	ALL
+} parameter_id_t;
+
+typedef enum {
+	SSH,
+	TELNET,
+	UNDEFINED
+}type_connection_t;
+typedef union {
+	long long int_param; //pour le port
+	char str_param[STR_MAX];// pour le username, password, login et 
+	bool flag_param; //pour le dryrun
+	type_connection_t connection_param; //pour le type de connection
+} data_wrapper_t;
+
+typedef struct {
+	parameter_id_t parameter_type;
+	data_wrapper_t parameter_value;
+}parameter_t;
